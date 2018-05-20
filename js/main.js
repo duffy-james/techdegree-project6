f8f5f8237404528c4cb0cfef02b5c46a4f75f85b
James Duffy <jduffy2000@gmail.com>
2018-05-20 00:04:21 -0400

    Added javascript
    Included looping over text and matching to video

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-$('video').mediaelementplayer({
-  features: ['playpause', 'fullscreen', 'volume', 'progress'],
-  startLanguage: 'en',
+const text = document.querySelectorAll('span');
+const videoPlay = document.querySelector('video');
+
+videoPlay.addEventListener('timeupdate', () => {
+
+    const currentVideo = videoPlay.currentTime;
+    for (let i = 0, s = text.length; i < s; i++) {
+      if (currentVideo >= +text[i].getAttribute('data-start') &&
+          currentVideo <= +text[i].getAttribute('data-end')) {
+          text[i].className = 'current';
+      } else {
+        text[i].className = '';
+      }
+    }
 });
+
+// textWrapper.addEventListener('click', () => {
+//
+//   for (let i = 0, s = text.length; i < s; i++) {
+//     if (currentVideo == text[i].getAttribute('data-start')) {
+//       const currentVideo = videoPlay.currentTime;
+//       currentVideo.play();
+//     }
+//   }
+//
+// });
