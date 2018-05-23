6bfd0b832ee0c28318962b216abed203d1d8c4e3
James Duffy <jduffy2000@gmail.com>
2018-05-22 21:28:46 -0400

    Figured out cross browser isssue
    Added a snipper of jquery to solve cross browser issues. Safari, firefox, and chrome all now work with the player.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-const textBlock = document.querySelectorAll('span');
+/*
+
+Initiate player using jquery (addresses Safari bug)
+
+*/
+
+$('video').mediaelementplayer({
+  features: ['playpause', 'progress', 'volume', 'fullscreen'],
+  startLanguage: 'en',
+});
+
+/*
+
+Define constants in code
+
+*/
+
+const textBlock = document.querySelectorAll('.textWrapper span');
 const videoPlay = document.querySelector('video');
+
+/*
+
+Loop through captions to highlight text as video player
+
+*/
 
 videoPlay.addEventListener('timeupdate', () => {
 
     const currentVideo = videoPlay.currentTime;
-    for (let i = 0, s = textBlock.length; i < s; i++) {
+
+    for (let i = 0; i < textBlock.length; i++) {
       if (currentVideo >= +textBlock[i].getAttribute('data-start') &&
           currentVideo <= +textBlock[i].getAttribute('data-end')) {
           textBlock[i].className = 'current';
@@ -14,8 +38,13 @@ videoPlay.addEventListener('timeupdate', () => {
     }
 });
 
-for (let i = 0; i < textBlock.length; i++) {
+/*
 
+Allow text to be clickable, jump to specific section in videoPlay
+
+*/
+
+for (let i = 0; i < textBlock.length; i++) {
   textBlock[i].addEventListener("click", () => {
     let start = parseFloat(textBlock[i].getAttribute('data-start'));
     videoPlay.currentTime = start;
